f085b48556c5d2550607ddca75789fd244ba00b8
ramos-angie <angie.ramos@anima.edu.uy>
2025-06-13 20:18:26 -0300

    ANGIE
    tengo que buscar el error en el jsx e importar al app js aun así no se ve nada je

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/ui/Navbar/Navbar";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Hero from "./components/Home/HeroSection";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -1,3 +1,31 @@
 import React from "react";
 import "./HeroSection.css";
-import heroImage from "../assets/hero.jpg";
+import heroImage from "./assets/imagen_horizontal_hero.jpg";
+
+const Hero = () => {
+  return (
+    <section
+      className="hero-section"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        textAlign: "center",
+  )}>
+      <div className="hero-content">
+        <h1>Buscá tu película ahora</h1
+        <div className="search-box">
+          <input type="text" placeholder="Buscar..." />
+          <button>Buscar</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
