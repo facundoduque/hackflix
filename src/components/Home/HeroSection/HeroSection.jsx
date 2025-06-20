9f8c48932269bf6a6a92f397b1064bd95fb13fd7
ramos-angie <angie.ramos@anima.edu.uy>
2025-06-20 18:39:18 -0300

    logre que el buscador este en e medio de la  tv

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -3,26 +3,9 @@ import "./HeroSection.css";
 
 export const HeroSection = () => {
   return (
-    <section
-      className="hero-section"
-      style={{
-        backgroundImage: `url(/imagen_horizontal_hero.jpg)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100dvh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white",
-        textAlign: "center",
-        }}
-        >
-      <div className="hero-content">
-        <h1>Buscá tu película ahora</h1>
-        <div className="search-box">
-          <input type="text" placeholder="Buscar..." />
-          <button>Buscar</button>
-        </div>
+    <section className="hero-section">
+      <div className="input-overlay">
+        <input type="text" placeholder="Buscar..." />
       </div>
     </section>
   );
