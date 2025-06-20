3090edf076007617a4e8e27bae9cf56394ac4ffb
ramos-angie <angie.ramos@anima.edu.uy>
2025-06-20 19:48:14 -0300

    quedo en el lugar falta cambiar la fuente a una más linda

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -4,8 +4,9 @@ import "./HeroSection.css";
 export const HeroSection = () => {
   return (
     <section className="hero-section">
-      <div className="input-overlay">
-        <input type="text" placeholder="Buscar..." />
+      <div className="tv-content">
+        <h1>¿Qué verás hoy?</h1>
+        <input type="text" placeholder="Hoy veré..." />
       </div>
     </section>
   );
