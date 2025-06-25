655a4ae50b6423dad26389ecaee914338fe174b7
MicaDorado <micadorado2016@gmail.com>
2025-06-24 21:37:18 -0300

    cambios mica

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { HeroSection } from "./components/Home/HeroSection/HeroSection";
 import { MoviesSection } from "./components/Home/MoviesSection/MoviesSection";
 import "./App.css";
 import Footer from "./components/ui/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +17,13 @@ function App() {
       <HeroSection />
       <MoviesSection />
       <Footer />
+
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+    </Routes>
+
+
     </>
   );
 }
