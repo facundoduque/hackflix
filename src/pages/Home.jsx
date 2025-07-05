f4940b94dce28222136752b1a1172ef03f3765da
ramos-angie <angie.ramos@anima.edu.uy>
2025-07-05 18:19:05 -0300

    Update Home.jsx
    quedaba en home una busqueda undefined y la quite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,12 @@ import Navbar from "../components/ui/Navbar/Navbar";
 import HeroSection from "../components/Home/HeroSection/HeroSection";
 import MoviesSection from "../components/Home/MoviesSection/MoviesSection";
 import Footer from "../components/ui/Footer/Footer";
-import SearchInput from "../components/ui/SearchInput/SearchInput.jsx";
-import SearchResults from "../components/SearchResults.jsx";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <SearchResults />
       <MoviesSection />
       <Footer />
     </div>
